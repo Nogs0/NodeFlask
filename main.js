bfb7e3b3f4e8cb04949e5999a4a4787b5d723e4a
GreenShimada <matheus_jonnas@proton.me>
2024-12-05 17:53:16 -0300

    isso ai

--- main.js
+++ main.js
@@ -4,22 +4,43 @@ const hostname = '127.0.0.1';
 const port = 3001;
 
 const server = http.createServer(async (req, res) => {
-  http.get(`http://127.0.0.1:5000/analyze`, (resFlask) => {
+  const youtubeUrl = 'https://www.youtube.com/watch?v=F_ukcJokV0g';
+
+  const flaskPath = `/analyze?url=${encodeURIComponent(youtubeUrl)}`;
+
+  const options = {
+    hostname: '127.0.0.1',
+    port: 5000,
+    path: flaskPath,
+    method: 'GET',   
+  };
+
+  const reqFlask = http.request(options, (resFlask) => {
     let rawData = '';
     resFlask.on('data', (chunk) => {
       rawData += chunk;
     });
 
-    resFlask.on('end', async () => {
+    resFlask.on('end', () => {
       try {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
+        res.statusCode = resFlask.statusCode;
+        res.setHeader('Content-Type', 'application/json');
         res.end(rawData);
       } catch (e) {
         console.error(e.message);
+        res.statusCode = 500;
+        res.end('Internal Server Error');
       }
     });
-  })
+  });
+
+  reqFlask.on('error', (e) => {
+    console.error(`Problem with request: ${e.message}`);
+    res.statusCode = 500;
+    res.end('Error connecting to Flask server');
+  });
+
+  reqFlask.end();
 });
 
 server.listen(port, hostname, () => {
--- main.py
+++ main.py
@@ -2,22 +2,28 @@ from flask import Flask, request, jsonify
 from textblob import TextBlob
 from yt import *
 
-
 app = Flask(__name__)
 
-@app.route('/analyze', methods=['POST'])
+LINK_VALIDO = "https://www.youtube.com/watch?"
+
+@app.route('/analyze', methods=['GET'])
 def analyze_text():
-    data = request.get_json()
-    url = data.get('url', '')
+    url = request.args.get('url')  
 
     if not url:
         return jsonify({"error": "Parâmetro 'url' é necessário!"}), 400
 
-    video_id = url.split("v=")[-1]
-    comentarios = Get_Comments(video_id=video_id)
+    url_split = url.split("v=")
+    url_youtube = url_split[0]
+    if url_youtube != LINK_VALIDO:
+        return {
+            "text": "Insira um link válido."
+        }
+    video_id = url_split[-1]
+    comentarios = Get_Comments(video_id=video_id)  
 
     results = []
-    for i, comentario in  enumerate(comentarios):
+    for i, comentario in enumerate(comentarios):
         analysis = TextBlob(comentario)
         result = {
             "index": i,
@@ -29,8 +35,5 @@ def analyze_text():
 
     return jsonify(results)
 
-@app.route('/analyze', methods=['GET'])
-def dica():
-    return "<p>O link do video precisa ser enviado no corpo da requisição usando o parâmetro \'url\'. </p>"
 if __name__ == '__main__':
     app.run(port=5000)
